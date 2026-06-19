1f245c1dd69f7a7134521fad68971548bd44f04c
Kareem-Kareem-kareem <kareemabidin1@gmail.com>
2026-06-19 19:14:07 +0300

    Update RLDisasters.h

--- src/RLDisasters.h
+++ src/RLDisasters.h
@@ -6,37 +6,11 @@
 #include <chrono>
 #include <mutex>
 
-struct PluginConfig {
-    bool masterEnabled = false;
-    bool biggerFieldMode = false;
-    float fieldScaleMultiplier = 2.0f;
-    bool quickRumbleEnabled = false;
-    float rumbleInterval = 1.0f;
-    bool infiniteBoost = false;
-    bool customGravity = false;
-    float gravityValue = -650.0f;
-    bool showHud = true;
-    bool allowSpikes = true;
-    bool allowPlunger = true;
-    bool allowMagnet = true;
-    bool allowBoot = true;
-    bool allowGrapple = true;
-    bool allowFreeze = true;
-    bool allowTornado = true;
-    bool allowTeleport = true;
-    bool allowPowerhit = true;
-    bool allowDisruptor = true;
-    float carScaleModifier = 0.5f;
-    bool independentBallScale = false;
-    float ballScaleModifier = 1.0f;
-};
-
 class RLDisasters : public BakkesMod::Plugin::BakkesModPlugin, public BakkesMod::Plugin::PluginWindow
 {
 public:
     void onLoad() override;
     void onUnload() override;
-    void OnTick(std::string eventName);
     void SetImGuiContext(uintptr_t ctx) override;
     void RenderCanvasHUD(CanvasWrapper canvas);
     
@@ -51,18 +25,15 @@ public:
     void OnClose() override;
 
 private:
-    PluginConfig config;
-    std::chrono::steady_clock::time_point lastTickTime;
-    float cumulativeRumbleTimer = 0.0f;
     bool isWindowOpen = false;
+    float cumulativeRumbleTimer = 0.0f;
+    std::chrono::steady_clock::time_point lastTickTime;
+    
     std::vector<std::string> diagnosticLogs;
     std::mutex logMutex;
-    float lastGravityValue = 0.0f;
-    bool lastGravityState = false;
     
     void AddLog(const std::string& message);
-    void ApplyFieldScale();
-    void ResetScales();
-    std::vector<std::string> BuildActiveItemPool();
+    void UpdateScaleTransformations();
+    void HandleRumbleInjection(float deltaTime);
     void DrawInterfaceLayout();
 };
