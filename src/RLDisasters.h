d928710904f2e2c2305aa1bc2e48df4253838bd7
Kareem-Kareem-kareem <kareemabidin1@gmail.com>
2026-06-21 20:14:35 +0300

    Update RLDisasters.h

--- src/RLDisasters.h
+++ src/RLDisasters.h
@@ -2,8 +2,9 @@
 #include "bakkesmod/plugin/bakkesmodplugin.h"
 #include "bakkesmod/plugin/pluginwindow.h"
 #include "bakkesmod/wrappers/GameEvent/ServerWrapper.h"
-#include "IMGUI/imgui.h"
+#include "imgui.h" 
 #include <string>
+#include <vector>
 
 class RLDisasters : public BakkesMod::Plugin::BakkesModPlugin, public BakkesMod::Plugin::PluginSettingsWindow
 {
@@ -11,25 +12,20 @@ public:
     void onLoad() override;
     void onUnload() override;
 
-    // Settings Window Interface implementation
     void RenderSettings() override;
     std::string GetPluginName() override;
 
 private:
-    // Core Plugin State Flags
     bool pluginEnabled = false;
     float currentGravity = -650.0f;
     int currentRandomItemIndex = 1;
 
-    // Event Registration Hooks
     void HookEvents();
     void UnhookEvents();
 
-    // Event Receivers
     void OnRoundStart(std::string eventName);
     void OnGoalScored(std::string eventName);
 
-    // Modification Utility Methods
     void ChooseRandomRumbleItem();
     void ApplyGravitySettings();
     void ResetMatchState();
