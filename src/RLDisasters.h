2a7ad775aa0829e2c43e7559872bc690e898ec71
Kareem-Kareem-kareem <kareemabidin1@gmail.com>
2026-06-21 19:52:46 +0300

    Refactor RLDisasters class structure and methods

--- src/RLDisasters.h
+++ src/RLDisasters.h
@@ -1,45 +1,25 @@
 #pragma once
-
 #include "bakkesmod/plugin/bakkesmodplugin.h"
-#include "bakkesmod/plugin/pluginwindow.h"
-#include "bakkesmod/plugin/PluginSettingsWindow.h"
-#include "version.h"
+#include <string>
 
-constexpr auto plugin_version = stringify(VERSION_MAJOR) "." stringify(VERSION_MINOR) "." stringify(VERSION_PATCH) "." stringify(VERSION_BUILD);
-
-class RLDisasters : public BakkesModPlugin::BakkesModPluginID
+class RLDisasters : public BakkesMod::Plugin::BakkesModPlugin
 {
+public:
+    void onLoad() override;
+    void onUnload() override;
+
 private:
     bool pluginEnabled = false;
     float currentGravity = -650.0f;
-    std::string currentRoundItem = "Spikes";
-    bool isMatchRunning = false;
+    int currentRandomItemIndex = 1;
 
-    const std::vector<std::string> rumbleItems = {
-        "GrapplingHook",
-        "Spikes",
-        "Magnet",
-        "Freeze",
-        "Boot",
-        "Haymaker",
-        "Tornado",
-        "Swapper",
-        "Disruptor",
-        "PowerHitter",
-        "Plunger"
-    };
+    void HookEvents();
+    void UnhookEvents();
 
-public:
-    virtual void onLoad() override;
-    virtual void onUnload() override;
-
-    void OnMatchStart(std::string eventName);
-    void OnMatchEnd(std::string eventName);
+    void OnRoundStart(std::string eventName);
     void OnGoalScored(std::string eventName);
-    void OnTick(std::string eventName);
 
-    void ResetMatchState();
+    void ChooseRandomRumbleItem();
     void ApplyGravitySettings();
-    void SelectNextRoundItem();
-    void EnforcePersistentItem();
+    void ResetMatchState();
 };
