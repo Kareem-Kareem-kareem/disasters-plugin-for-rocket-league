9f881513c628b293f8a5cdbcbd201cfa0988cbaa
Kareem-Kareem-kareem <kareemabidin1@gmail.com>
2026-06-21 19:03:56 +0300

    Update RLDisasters.h

--- src/RLDisasters.h
+++ src/RLDisasters.h
@@ -18,19 +18,18 @@ private:
     float currentGravity     = -650.0f;
     float gravityStepPerGoal = 100.0f;
     int   lastPhysicsFrame   = -1;
-    
-    std::string persistentItemCmd = "spikes"; 
+    bool  lastTickHadPickup  = false;
 
     void HookEvents();
     void UnhookEvents();
 
     void OnMatchStarted(std::string eventName);
     void OnGoalScored(std::string eventName);
-    void OnCarSpawned(std::string eventName);
+    void OnKickoffStart(std::string eventName);
     void OnTick(std::string eventName);
 
     void GrowBall();
-    void UpdateRumbleCVars();
+    void TickRumbleTracking();
     void ApplyLowGravityGoals();
 
     void ResetAll();
