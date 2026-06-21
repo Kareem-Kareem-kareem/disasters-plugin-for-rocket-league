ce750ddf46b3d861781e10a1b25de650797cc0d7
Kareem-Kareem-kareem <kareemabidin1@gmail.com>
2026-06-21 19:15:37 +0300

    Update RLDisasters.h

--- src/RLDisasters.h
+++ src/RLDisasters.h
@@ -9,26 +9,21 @@ public:
     void onUnload() override;
 
 private:
-    bool growingBallOn      = false;
     bool persistentRumbleOn = false;
     bool lowGravityGoalsOn  = false;
 
     int   goalsScored        = 0;
-    float ballScale          = 1.0f;
     float currentGravity     = -650.0f;
     float gravityStepPerGoal = 100.0f;
     int   lastPhysicsFrame   = -1;
-    bool  lastTickHadPickup  = false;
 
     void HookEvents();
     void UnhookEvents();
 
     void OnMatchStarted(std::string eventName);
     void OnGoalScored(std::string eventName);
-    void OnKickoffStart(std::string eventName);
     void OnTick(std::string eventName);
 
-    void GrowBall();
     void TickRumbleTracking();
     void ApplyLowGravityGoals();
 
