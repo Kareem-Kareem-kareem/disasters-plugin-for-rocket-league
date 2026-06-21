c980771ae2a8dbcd5f74c05aa4664584c9288ae6
Kareem-Kareem-kareem <kareemabidin1@gmail.com>
2026-06-21 19:59:20 +0300

    Update RLDisasters.h

--- src/RLDisasters.h
+++ src/RLDisasters.h
@@ -1,32 +1,25 @@
 #pragma once
 #include "bakkesmod/plugin/bakkesmodplugin.h"
 #include <string>
-#include <vector>
 
 class RLDisasters : public BakkesMod::Plugin::BakkesModPlugin
 {
-private:
-    bool pluginEnabled = false;
-    float currentGravity = -650.0f;
-    std::string currentRoundItem = "Spikes";
-    bool isMatchRunning = false;
-
-    const std::vector<std::string> rumbleItems = {
-        "GrapplingHook", "Spikes", "Magnet", "Freeze", "Boot", 
-        "Haymaker", "Tornado", "Swapper", "Disruptor", "PowerHitter", "Plunger"
-    };
-
 public:
     void onLoad() override;
     void onUnload() override;
 
-    void OnMatchStart(std::string eventName);
-    void OnMatchEnd(std::string eventName);
-    void OnGoalScored(std::string eventName);
-    void OnTick(std::string eventName);
+private:
+    bool pluginEnabled = false;
+    float currentGravity = -650.0f;
+    int currentRandomItemIndex = 1;
 
-    void ResetMatchState();
+    void HookEvents();
+    void UnhookEvents();
+
+    void OnRoundStart(std::string eventName);
+    void OnGoalScored(std::string eventName);
+
+    void ChooseRandomRumbleItem();
     void ApplyGravitySettings();
-    void SelectNextRoundItem();
-    void EnforcePersistentItem();
+    void ResetMatchState();
 };
