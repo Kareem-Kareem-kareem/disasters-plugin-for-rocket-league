170d95609b99a6b96a1c4667aa406ab2f6a194ff
Kareem-Kareem-kareem <kareemabidin1@gmail.com>
2026-06-22 10:46:01 +0300

    Update RLDisasters.h

--- src/RLDisasters.h
+++ src/RLDisasters.h
@@ -1,32 +1,38 @@
 #pragma once
 #include "bakkesmod/plugin/bakkesmodplugin.h"
-#include "bakkesmod/plugin/pluginwindow.h"
-#include "bakkesmod/wrappers/GameEvent/ServerWrapper.h"
-#include "imgui.h" 
 #include <string>
-#include <vector>
 
-class RLDisasters : public BakkesMod::Plugin::BakkesModPlugin, public BakkesMod::Plugin::PluginSettingsWindow
+class RLDisasters : public BakkesMod::Plugin::BakkesModPlugin
 {
 public:
     void onLoad() override;
     void onUnload() override;
 
-    void RenderSettings() override;
-    std::string GetPluginName() override;
-
 private:
-    bool pluginEnabled = false;
-    float currentGravity = -650.0f;
-    int currentRandomItemIndex = 1;
+    bool growingBallOn      = false;
+    bool persistentRumbleOn = false;
+    bool lowGravityGoalsOn  = false;
+    bool chaosSpeedOn       = false;  // SetGameSpeed confirmed real in ServerWrapper.h
+
+    int   goalsScored        = 0;
+    float ballScale          = 1.0f;
+    float currentGravity     = -650.0f;
+    float gravityStepPerGoal = 100.0f;
+    float currentGameSpeed   = 1.0f;
+    int   lastPhysicsFrame   = -1;
+    bool  lastTickHadPickup  = false;
 
     void HookEvents();
     void UnhookEvents();
 
-    void OnRoundStart(std::string eventName);
+    void OnMatchStarted(std::string eventName);
     void OnGoalScored(std::string eventName);
+    void OnTick(std::string eventName);
 
-    void ChooseRandomRumbleItem();
-    void ApplyGravitySettings();
-    void ResetMatchState();
+    void GrowBall();
+    void TickRumbleTracking();
+    void ApplyLowGravityGoals();
+    void ApplyChaosSpeed();
+
+    void ResetAll();
 };
